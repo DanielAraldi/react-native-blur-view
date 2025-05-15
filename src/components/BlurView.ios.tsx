24ffbe5328f8aed9e9a3503256a3e81468e55d26
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-05-15 16:29:43 -0300

    feat(ios): add `radius`, `autoUpdate` and `isEnabled` properties for iOS `BlurView`

--- src/components/BlurView.ios.tsx
+++ src/components/BlurView.ios.tsx
@@ -1,12 +1,21 @@
-import { forwardRef, memo } from 'react';
+import { forwardRef, memo, useCallback } from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import { BlurViewView } from '../fabrics';
 import type { BlurViewIosProps, BlurViewIosType } from '../@types';
 import { globalStyles } from '../styles';
+import { clip } from '../utils';
 
 const BlurView = forwardRef<View, BlurViewIosProps>((props, ref) => {
-  const { type = 'light', style, ...rest } = props;
+  const {
+    type = 'light',
+    radius = 10,
+    isEnabled = true,
+    style,
+    ...rest
+  } = props;
+
+  const handleClipRadius = useCallback(() => clip(radius, 0, 100), [radius]);
 
   const overlayColors: Record<BlurViewIosType, string> = {
     'x-light': 'xlight',
@@ -32,11 +41,15 @@ const BlurView = forwardRef<View, BlurViewIosProps>((props, ref) => {
   };
 
   const overlayColor = overlayColors[type] || overlayColors.light;
+  const blurRadius = handleClipRadius();
 
   return (
     <BlurViewView
       ref={ref}
       overlayColor={overlayColor}
+      blurRadius={blurRadius}
+      enabled={isEnabled}
+      autoUpdate
       pointerEvents="none"
       style={StyleSheet.compose(globalStyles.container, style)}
       {...rest}
