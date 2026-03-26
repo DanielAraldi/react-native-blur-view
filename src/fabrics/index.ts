a6ed13db047226c6a07c522c4e720f5fb2040b9d
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2026-03-26 15:22:16 -0300

    fix: move components to unique single file and added example in the JSDoc them

--- src/fabrics/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Blur } from './BlurViewNativeComponent';
-export { default as Target } from './TargetViewNativeComponent';
-export { default as Vibrancy } from './VibrancyViewNativeComponent';
